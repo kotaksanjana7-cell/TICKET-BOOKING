16c620984691d0ca7aa1da254c184c850852ffc3
kotak sanjana <kotaksanjana@gmail.com>
2025-12-22 09:54:16 +0530

    done

--- src/routes/UserRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controller/UserController");
-const userController = new UserController();
-
-router.post("/add", userController.addUser);
-
-module.exports = router;
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRoute = require("./UserRoute");
-
-router.use("/user", userRoute);
-
-module.exports = router;
